4a0752b04bb2acf2dca21b101b75248f415ba1bb
Waleed Adel <waleed.developer222@gmail.com>
2025-11-14 22:31:43 +0200

    add onboarding screen , adjust splash screen

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,10 +1,135 @@
+import 'package:anamel/core/const/app_assets_path.dart';
+import 'package:anamel/core/routes/app_routing.dart';
+import 'package:anamel/core/styling/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
 
   @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _controller = PageController();
+  int currentPage = 0;
+
+  final onboardingItems = [
+
+    {
+      "image": AppAssetsPath.onboardingImage_1,
+      "title": "Welcome to Anamel"
+    },
+    {
+      "image": AppAssetsPath.onboardingImage_3,
+      "title": "Beautify your life with unique handmade products ",
+    },
+    {
+      "image": AppAssetsPath.onboardingImage_3,
+      "title": "crafted with love and care, made especially for you",
+    },
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    bool isLast = currentPage == onboardingItems.length - 1;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            // PageView
+            Expanded(
+              child: PageView.builder(
+                controller: _controller,
+                itemCount: onboardingItems.length,
+                onPageChanged: (index) {
+                  setState(() => currentPage = index);
+                },
+                itemBuilder: (_, index) {
+                  final item = onboardingItems[index];
+
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Image.asset(item["image"]!, height: 280),
+                        SvgPicture.asset(
+                          item["image"]!,
+                          height: 250.h,
+                          width: 300.w,
+                        ),
+
+                        SizedBox(height: 28.h),
+
+                        Text(
+                          item["title"]!,
+                          style: AppStyles.logoTitleStyle.copyWith(
+                            fontSize: 28.sp,
+                          ),
+                        ),
+                        SizedBox(height: 28.h),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            SizedBox(height: 24.h),
+
+            // Expanding Dots
+            SmoothPageIndicator(
+              controller: _controller,
+              count: onboardingItems.length,
+              effect: ExpandingDotsEffect(
+                dotHeight: 12,
+                dotWidth: 12,
+                expansionFactor: 3,
+                spacing: 6,
+                activeDotColor: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+
+            SizedBox(height: 32.h),
+
+            // Circular Button with Arrow / Check
+            GestureDetector(
+              onTap: () {
+                if (!isLast) {
+                  _controller.nextPage(
+                    duration: Duration(milliseconds: 400),
+                    curve: Curves.easeOut,
+                  );
+                } else {
+                  // go to login screen
+                  GoRouter.of(context).pushReplacement(AppRouting.login);
+                }
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                height: 60,
+                width: 60,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  isLast ? Icons.check : Icons.arrow_forward,
+                  size: 28,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 40.h),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,11 +1,9 @@
-import 'package:anamel/core/const/app_assets_path.dart';
 import 'package:anamel/core/routes/app_routing.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../core/styling/app_styles.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,75 +14,58 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  Future<void> initState() async {
+  void initState() {
     super.initState();
 
-    if (await _checkFirstTime()) {
-      // Navigate to onboarding screen
-      Future.delayed(Duration(seconds: 2), () {
+    _checkFirstTime();
+  }
+
+  void _checkFirstTime() {
+    Future.microtask(() async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      bool isFirstTime = prefs.getBool("is_first_time") ?? true;
+
+      if (isFirstTime) {
+        prefs.setBool("is_first_time", false);
+
+        // go to onboarding screen
         GoRouter.of(context).pushReplacement(AppRouting.onboarding);
-      });
-    } else {
-      _checkUser();
-    }
-  }
-
-  Future<bool> _checkFirstTime() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool isFirstTime = prefs.getBool("is_first_time") ?? true;
-
-    if (isFirstTime) {
-      prefs.setBool("is_first_time", false);
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  void _checkUser() {
-    final user = FirebaseAuth.instance.currentUser;
-    // return user != null ? true : false;
-
-    Future.delayed(Duration(seconds: 2), () {
-      if (user != null) {
-        GoRouter.of(context).pushReplacement(AppRouting.home);
       } else {
-        GoRouter.of(context).pushReplacement(AppRouting.login);
+        // check user login status
+        _checkUser();
       }
     });
   }
 
+  void _checkUser() {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      GoRouter.of(context).pushReplacement(AppRouting.home);
+    } else {
+      GoRouter.of(context).pushReplacement(AppRouting.login);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: Svg(AppAssetsPath.anamelCoverImage),
-          fit: BoxFit.fitHeight,
-        ),
-      ),
-
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Anamel",
-                style: AppStyles.logoTitleStyle.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              ),
-              Text(
-                "Made With Love ",
-                style: AppStyles.logoSubtitleStyle.copyWith(
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              ),
-            ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Anamel",
+            style: AppStyles.logoTitleStyle.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+            ),
           ),
-        ),
+          Text(
+            "Made With Love ",
+            style: AppStyles.logoSubtitleStyle.copyWith(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ],
       ),
     );
   }
